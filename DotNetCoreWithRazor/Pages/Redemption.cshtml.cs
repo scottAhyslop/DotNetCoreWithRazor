47358e635a5eb4f0181ca275a99281fc0a1f3e60
Scott Hyslop <sahyslop@gmail.com>
2021-07-18 15:24:18 -0400

    Add project files.

---
+++ DotNetCoreWithRazor/Pages/Redemption.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCoreApp.Pages
+{
+    public class RedemptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
